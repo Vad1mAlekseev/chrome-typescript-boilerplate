6968acf38f389fc14cba9708600b612282154fa3
Vadim Alekseev <Vad1mAlekseev2807@gmail.com>
2020-04-12 17:10:43 +0300

    preparation for publication

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -3,7 +3,7 @@ import _ from 'lodash';
 const app = document.querySelector('#app');
 if (app) {
   app.innerHTML = `
-    <h2>Hello world!!!</h2>
+    <h2>Hello world!</h2>
   `;
   chrome.alarms.onAlarm.addListener((ctx) => {
     console.log('alarm', ctx)
